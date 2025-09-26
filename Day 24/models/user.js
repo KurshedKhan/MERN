94bc691a5c0a23f9a1baa1b7073f0f51359882a2
KurshedKhan <kurshedkhanbhadra@gmail.com>
2025-09-26 11:02:59 +0530

    mern

---
+++ Day 24/models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  firstName : {
+    type : String,
+    required : [true,"First name is required"]
+  },
+  lastName : {
+    type : String,
+    required : [true,"Last name is required"]
+  },
+  email : {
+    type : String,
+    required : [true,"email is required"],
+    unique : true
+  },
+  password : {
+    type : String,
+    required : [true,"password is required"],
+  },
+  userType : {
+    type : String,
+    enum : ['Guest',"Host"],
+    default : 'Guest'
+  },
+});
+
+module.exports = mongoose.model('User',userSchema);
